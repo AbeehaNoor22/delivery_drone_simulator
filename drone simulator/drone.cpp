c9dba622f058cdec53d96a666f07fb7c9546e562
Aliza-Batool <alizazaidi2007@gmail.com>
2025-10-26 19:09:09 +0500

    Modified version 4

--- drone simulator/drone.cpp
+++ drone simulator/drone.cpp
@@ -1,55 +1,71 @@
-//delivery drone simulator
+//DELIVERY DRONE SIMULATOR
 #include<iostream>
 #include<cstdlib>
-#include<ctime>
-#include<chrono>
+#include<ctime>   //for srand and rand
+#include<chrono>  //for adding delay
 #include<thread>
+#include <limits>
 using namespace std;
-//function prototypes
+//FUNCTION PROTOTYPES
 bool startDay(int battery);
-int getWeather();
-bool checkObstacle();
-int delivery_time(char location);//*added this function
+int getWeather();  //randomly generates weather
+bool checkObstacle();  //randomly generates obstacles
+int delivery_time(char location);  //gives the delivery time
 bool deliverPackage(char location, int& battery, int drain_per_trip, int& success, int& failed, int& delayed);
 void displaySummary(int success, int failed, int delayed, int battery);
-bool loadCheck(int);//added this function
+bool loadCheck(int);  //verifies if the load is within limit
+
+//MAIN FUNCTION
 int main() {
 	//initializing variables
 	int batteryLevel = 100;
 	const int drain_per_trip = 15;
-	int success = 0, failed = 0, delayed = 0;
+	int success = 0, failed = 0, delayed = 0;  //delivery counters
 	srand(time(0));
 
 	if (startDay(batteryLevel) == true) {
 		this_thread::sleep_for(chrono::seconds(1));
 		cout << "Starting deliveries!\n" << endl;
 		cout << "------------------------------------\n";
-		while (true) //*changed the loop
+		while (true)
 		{
 			char location;
 			int load;
-			cout << "Enter the delivery package load.\n";
+			//taking load input
+			cout << "Enter the delivery package load in kg: ";
 			cin >> load;
+
+			//input validation
+			while (cin.fail()) {
+				cout << "Invalid input! Please enter a numeric value: ";
+				cin.clear(); // clear error flag
+				cin.ignore(numeric_limits<streamsize>::max(), '\n'); // discard bad input
+				cin >> load; // ask again
+			}
 
 			if (loadCheck(load) == true) {
 
-				cout << "Enter location(A/B/C) or enter X to exit. ";
+				cout << "Enter location(A/B/C) or enter X to exit. ";///location input
 				cin >> location;
 				cout << "------------------------------------\n";
+
+				//delivery location validation
 				if (location == 'A' || location == 'a' || location == 'B' || location == 'b' || location == 'C' || location == 'c') {
 					deliverPackage(location, batteryLevel, drain_per_trip, success, failed, delayed);
-					cout << "------------------------------------\n\n";
-					cout << "Time for this delivery will be " << delivery_time(location) << " minutes.\n";
+					//cout << "------------------------------------\n\n";
+					cout << "Time taken for this delivery was " << delivery_time(location) << " minutes.\n";
 					cout << "\n------------------------------------\n";
 				}
+				//exit condition
 				else if (location == 'x' || location == 'X') {
-					cout << "End of Deliveries for today.\n";
+					cout << "\n\nEnd of Deliveries for today.\n";
 					displaySummary(success, failed, delayed, batteryLevel);
 					break;
 				}
 				else { cout << "Invalid Location entered. Please try again.\n"; }
 			}
 			else {
+				//heavy load warning
 				cout << "Please reduce weight and try again.\n";
 				cout << "------------------------------------\n";
 			}
@@ -61,21 +77,21 @@ int main() {
 
 	return 0;
 }
-//function definitions
+//FUNCTION DEFINITIONS
 bool startDay(int battery) {
 	char start;
 	cout << "------------------------------------\n";
 	cout << "WELCOME TO DELIVERY DRONE SIMULATOR!\n";
-	this_thread::sleep_for(chrono::seconds(1));//added all times delays
+	this_thread::sleep_for(chrono::seconds(1));
 	cout << "Initial battery level: " << battery << "%" << endl;
 	cout << "------------------------------------\n\n";
 	this_thread::sleep_for(chrono::seconds(1));
-	cout << "\nStart delivering?(Y/N):\n ";
+	cout << "\nStart delivering?(Y/N):";
 	cin >> start;
-	if (start == 'Y' || start == 'y')//*modified this
+	if (start == 'Y' || start == 'y')
 	{
 		cout << "Preparing for delivery.Please wait.\n";
-		this_thread::sleep_for(chrono::seconds(2));
+		this_thread::sleep_for(chrono::seconds(1));
 		return true;
 	}
 	else {
@@ -83,6 +99,7 @@ bool startDay(int battery) {
 		return false;
 	}
 }
+//random weather generation
 int getWeather() {
 	cout << "Analyzing weather...\n";
 	this_thread::sleep_for(chrono::seconds(2));
@@ -101,6 +118,7 @@ int getWeather() {
 	}
 	return weather;
 }
+//random obstacle generation
 bool checkObstacle() {
 	cout << "Analyzing objects in the path...\n";
 	this_thread::sleep_for(chrono::seconds(2));
@@ -113,6 +131,7 @@ bool checkObstacle() {
 		return false;
 	}
 }
+//load check
 bool loadCheck(int load) {
 	if (load <= 50 && load > 0) {
 		cout << "The weight is within the load capacity.\nProceeding forward...\n";
@@ -121,16 +140,17 @@ bool loadCheck(int load) {
 		return true;
 	}
 	else if (load > 50) {
-		cout << "The weight is out of the load capacity\n";
+		cout << "The weight is out of the load capacity.\n";
 		return false;
 	}
-	else { cout << "Invalid load entered"; return false; }
+	else { cout << "Invalid load entered. "; return false; }
 
 }
-int delivery_time(char location) //added ths function
+//time calculation
+int delivery_time(char location) 
 {
-	cout << "Estimating the total delivery time...\n";
-	this_thread::sleep_for(chrono::seconds(3));
+	cout << "\n\nEstimating the total delivery time...\n";
+	this_thread::sleep_for(chrono::seconds(2));
 	if (location == 'A' || location == 'a') {
 		return 30;
 	}
@@ -142,20 +162,41 @@ int delivery_time(char location) //added ths function
 	}
 	else { cout << "Could not estimate time\n"; }
 }
+//deliver decision
 bool deliverPackage(char location, int& battery, int drain_per_trip, int& success, int& failed, int& delayed) {
 
 	cout << "Delivering to " << location << " \n";
-
+	//function calls
 	int weather = getWeather();
-	//weather based decision making module
-	if (weather == 3) {
-
-		cout << "Flight delayed.\n";
-		delayed++;
-		success++;
+	bool obstacle = checkObstacle();
+	//decision logic
+	
+	//obstacle check
+	if (obstacle == true) {
+		cout << "Obstacle detected. Rerouting path...\n";
+		this_thread::sleep_for(chrono::seconds(1));
+		if (weather != 3) {
+			cout << "Extra battery used during reroute.\n";
+			battery -= (drain_per_trip + 5);
+		}
 
 	}
-	else if (weather == 2 && battery < 40) {
+	else {
+		cout << "No obstacle detected\n";
+
+		if (weather != 3) {
+			battery -= drain_per_trip;
+		}
+	}
+
+	if (weather == 3) {//rainy
+
+		cout << "Flight delayed due to Rain.\n";
+		delayed++;
+		cout << "Remaining battery: " << battery << "%\n";
+		return false;
+	}
+	else if (weather == 2 && battery < 40) {//windy but low battery
 		cout << "Weather is too Windy and low battery.\n";
 		cout << "Returning to base to Recharge\n";
 		battery += 10;
@@ -163,36 +204,29 @@ bool deliverPackage(char location, int& battery, int drain_per_trip, int& succes
 			battery = 100;
 		}
 		delayed++;
-
+		cout << "Remaining battery: " << battery << "%\n";
+		return false;
 	}
-	else if (weather == 2 && battery >= 40) {
-		cout << "Weather is windy but battery is sufficient.\nProceeding with caution.\n";
+	else if (weather == 2 && battery >= 40)//windy but enough battery
+	{
+		cout << "Weather is Windy but battery is sufficient. Proceeding with caution.\n";
 		success++;
 	}
-	//obstacle based decision making module
-	bool obstacle = checkObstacle();
-	if (obstacle == true) {
-		cout << "Obstacle detected. Rerouting path...\n";
-		this_thread::sleep_for(chrono::seconds(2));
-		cout << "Extra battery used during reroute.\n";
-		battery -= (drain_per_trip + 5);
-	}
-	else {
-		cout << "No obstacle detected.\n";
-		battery -= drain_per_trip;
-	}
-	//battery based decision making module
+	//battery check
 	if (battery <= 0) {
 		cout << "Battery exhausted! Delivery failed.\n";
 		failed++;
-
+		cout << "Remaining battery: " << battery << "%\n";
+		return false;
 	}
-	cout << "Package successfully delivered to Location. " << location << endl;
+	//successful delivery
+	cout << "Package successfully delivered to Location " << location << endl;
 	cout << "Remaining battery: " << battery << "%\n";
 	success++;
 	return true;
 
 }
+//mission summary
 void displaySummary(int success, int failed, int delayed, int battery) {
 	cout << "------------------------------------\n";
 	cout << "         DAY SUMMARY" << endl;
